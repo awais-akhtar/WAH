364ff9eec1e1789e5256639f334f877321e31b4b
Awais akhtar <awais918@gmail.com>
2023-04-06 10:30:42 +0500

    charts

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,6 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True #env('DEBUG')
 
